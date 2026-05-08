ad4af83992f2bc3257334e47ed69d91b6ef8f541
amaurys30 <amaurys.castro@cecar.edu.co>
2026-05-08 18:17:29 -0500

    Se agrego navbar a camp

--- src/app/proyectos/campamentos/page.js
+++ src/app/proyectos/campamentos/page.js
@@ -13,7 +13,7 @@ import Footer from '@/components/layout/Footer'
 export default function CampamentosPage() {
   return (
     <>
-
+      <Navbar/>
       <CampHero />
       <CampAbout/>
       <CampGallery/>
